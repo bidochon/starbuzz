b1e967f08b8a1a4127d30e35b67fda32e3a564fb
JeanBilheux <bilheuxjm@ornl.gov>
2015-06-29 21:51:43 -0400

    Added project completed

---
+++ app/src/main/java/com/example/ventus/starbuzz/TopLevelActivity.java
@@ -0,0 +1,36 @@
+package com.example.ventus.starbuzz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import java.util.List;
+
+
+public class TopLevelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+
+        OnItemClickListener itemClickListener = new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+
+}
